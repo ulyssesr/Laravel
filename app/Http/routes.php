7d0fe3b34307e5bc4f50482825f0008bf04fdc76
Ulysses <ulyssesr@yahoo.com>
2016-03-11 12:12:31 -0800

    init
    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+	return view('welcome');
+});
+
+Route::get('about', function () {
+	return view('pages.about');
+});
+
+Route::get('cards', 'CardsController@index');
+
+Route::get('cards/{card}', 'CardsController@show');
+
+Route::post('cards/{card}/notes', 'NotesController@store');
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="container">
+        <div class="content">
+            <div class="title">About Page Here</div>
+        </div>
+    </div>
+
+@stop
